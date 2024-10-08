61b4ff49a634c7e092a991d2b5db28f0b6cd6876
User <gk_qube@mail.ru>
2024-10-08 10:28:21 +0700

    8/10 (3) - Начало

--- main.py
+++ main.py
@@ -47,28 +47,40 @@ class DocumentManagementSystem:
         if search_term:
             found_docs = [doc for doc in self.documents if search_term.lower() in doc["title"].lower()]
             if found_docs:
-                self.document_listbox.delete(0, tk.END)
+                # Открываем окно с содержимым первого найденного документа
+                self.document_listbox.delete(0, tk.END)  # Очищаем список
                 for doc in found_docs:
                     self.document_listbox.insert(tk.END, doc["title"])
+
+                # Показать сообщение о том, что документ найден
+                messagebox.showinfo("Документ найден",
+                                    f"Найден(ы) документ(ы):\n{', '.join(doc['title'] for doc in found_docs)}")
+                self.view_document_content(event=None,
+                                           document=found_docs[0])  # Отображаем содержимое первого найденного документа
             else:
                 messagebox.showinfo("Результаты поиска", "Документы не найдены.")
         else:
             messagebox.showwarning("Ошибка", "Введите ключевое слово для поиска.")
 
-    def view_document_content(self, event):
-        selected_index = self.document_listbox.curselection()
-        if selected_index:
-            selected_doc = self.documents[selected_index[0]]
-            content_window = tk.Toplevel(self.root)
-            content_window.title(selected_doc["title"])
-
-            content_label = tk.Label(content_window, text=selected_doc["content"], padx=10, pady=10)
-            content_label.pack()
-
-            close_button = tk.Button(content_window, text="Закрыть", command=content_window.destroy)
-            close_button.pack(pady=5)
+    def view_document_content(self, event=None, document=None):
+        if document is None:
+            selected_index = self.document_listbox.curselection()
+            if selected_index:
+                selected_doc = self.documents[selected_index[0]]
+            else:
+                messagebox.showwarning("Ошибка", "Выберите документ для просмотра.")
+                return
         else:
-            messagebox.showwarning("Ошибка", "Выберите документ для просмотра.")
+            selected_doc = document  # Если документ передан в функцию
+
+        content_window = tk.Toplevel(self.root)
+        content_window.title(selected_doc["title"])
+
+        content_label = tk.Label(content_window, text=selected_doc["content"], padx=10, pady=10)
+        content_label.pack()
+
+        close_button = tk.Button(content_window, text="Закрыть", command=content_window.destroy)
+        close_button.pack(pady=5)
 
 
 if __name__ == "__main__":
